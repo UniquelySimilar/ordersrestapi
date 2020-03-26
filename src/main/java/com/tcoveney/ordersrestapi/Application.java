1fc1618d577150c349815763643127a77e988f70
UniquelySimilar <consciousexplorer@gmail.com>
2020-03-26 14:40:33 -0600

    disabled hibernate autoconfiguration

--- src/main/java/com/tcoveney/ordersrestapi/Application.java
+++ src/main/java/com/tcoveney/ordersrestapi/Application.java
@@ -2,10 +2,11 @@ package com.tcoveney.ordersrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 import com.tcoveney.ordersrestapi.config.AppConfig;
 
-@SpringBootApplication
+@SpringBootApplication(exclude=HibernateJpaAutoConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
